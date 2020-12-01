9fa260af3dbd445e5ceeb979e6760046bc2a4c43
jgalinha <j.b.galinha@gmail.com>
2020-12-01 11:39:31 +0000

    Add while loop for menu with option to quit

--- main.sh
+++ main.sh
@@ -1,37 +1,46 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Variável que define o nome do ficheiro a localizar por defeitos
 SWITCHBOARD_FILE="switchboard.zip"
+# Variável que define quais são as pastas necessárias para o funcionamento do 
 DIRS=("scripts" "corpus" "corpus_txt" "corpus_info" "words_dic" "sentences_dic")
+FILE_LOCATED=false
+DIR_STRUCT_OK=false
 
-
+# Função que procura o ficheiro do switchboard e se não encontrar solicita a sua
+# localização
 SwitchBoard(){
     echo -e "🔎 Localizando o ficheiro $SWITCHBOARD_FILE\n"
-    if [ -f $SWITCHBOARD_FILE ]; then
+    if [ -f $SWITCHBOARD_FILE ]; then # Verifica se o fich. se encontra na dir
         echo "✅ Ficheiro $SWITCHBOARD_FILE encontrado"
+        FILE_LOCATED=true
     else
         while :
         do
             echo -e "❌ Ficheiro não encontrado\n"
             echo "Insira o nome do ficheiro do switchboard!"
             echo "Ficheiros .zip na directoria $PWD"
-            for file in ./*.zip
+            for file in ./*.zip # mostra todos os fich. disp. na dir.
             do
                 echo " -> ${file##*/}"
             done
-            echo -e "Insira do nome do ficheiro do switchboard : "
+            echo -e "Insira a localização do ficheiro do switchboard : "
             read ficheiro
-            if [ -f $ficheiro  ]; then
+            if [ -f $ficheiro  ]; then # verifica se o fich. existe
                 clear
                 echo "✅ Ficheiro $ficheiro encontrado"
                 SWITCHBOARD_FILE=$ficheiro
+                FILE_LOCATED=true
                 break
             fi  
-            clear
+            clear # limpa a consola
         done
     fi
 }
 
+# Função para verificar se existe a estrutura de dir. a usar procura uma a uma
+# caso não existam cria as mesmas
 CheckFolderStructure(){
     echo -e "\n🛠  Incializando a verificação da estrutura de diretorias\n"
     for dir in ${DIRS[@]};
@@ -46,9 +55,29 @@ CheckFolderStructure(){
         fi
     done
     echo " Diretorias 🆗"
+    DIR_STRUCT_OK=true
 }
 
-echo "Bem vindo ao gerador de corpus para o Eugénio V3!"
-echo "-------------------------------------------------"
-CheckFolderStructure
-SwitchBoard
+while true; do
+    echo "Bem vindo ao gerador de corpus para o Eugénio V3!"
+    echo "-------------------------------------------------"
+    if ! $DIR_STRUCT_OK; then
+        CheckFolderStructure
+    else
+        echo "Diretorias 🆗"
+    fi
+    if ! $FILE_LOCATED; then
+        SwitchBoard
+    else
+        echo "Ficheiro do switchboard 🆗"
+    fi
+    echo "Escolha a opção pretendida"
+    echo "--------------------------"
+    echo " 0 - sair"
+    read -p " -> " option
+    case $option in
+        0) break
+    esac
+done
+
+echo "FIM"
