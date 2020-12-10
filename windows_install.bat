9568dd5d35170c0a2741a6f5f6b88006110e26d7
José Galinha <j.b.galinha@gmail.com>
2020-12-10 12:14:23 +0000

    Copy files to eugenio folder in windows

--- windows_install.bat
+++ windows_install.bat
@@ -1,24 +1,51 @@
 ::===============================================================
-:: Script de instalaÃ§Ã£o dos ficheiros do EugÃ©nio V3 no windows
+:: Script de instalação dos ficheiros do Eugénio V3 no windows
 ::
 ::===============================================================
 @ECHO OFF
 :: Windows version check
-IF NOT "%OS%"=="Windows_NT" GOTO Syntax
+IF NOT "%OS%"=="Windows_NT" GOTO NotWindows
 
 :: Comando para permitir os caracteres unicode
-chcp 65001
-SET DIR=EugÃ©nio
+chcp 1252
+SET DIR=Eugénio
+SET CONTROL=false
 
 :CheckDirectory
 IF EXIST "%PROGRAMFILES%\%DIR%" (
-   ECHO Diretoria '%PROGRAMFILES%\%DIR%' detectada iniciando copia de ficheiros
+   SET DEST=%PROGRAMFILES%\%DIR%
+   SET CONTROL=true
+   GOTO Copy
+) ELSE (
+  GOTO NotFound
+)
+IF EXIST "%PROGRAMFILES(x86)%\%DIR%" (
+   SET DEST=%PROGRAMFILES(x86)%\%DIR%
+   SET CONTROL=true
+   GOTO Copy
+) ELSE (
+  GOTO NotFound
 )
 
+IF %CONTROL% equ false (
+   GOTO NotFound
+)
 
+:NotFound
+ECHO "Pasta do Eugénio não encontrada! Instale o Eugénio e volte a tentar."
 GOTO Exit
 
-:Syntax
-ECHO Este ficheiro Ã© para ser usado num SO Windows
+:Copy
+ECHO Diretoria '%DEST%' detectada iniciando copia de ficheiros
+COPY /y ".\words_dic\words.txt" "%DEST%\geral.pal"
+COPY /y ".\words_dic\words_pairs.txt" "%DEST%\geral.par"
+COPY /y ".\sentences_dic\sentences.txt" "%DEST%\geral.frs"
+COPY /y ".\sentences_dic\sentences_pairs.txt" "%DEST%\geral.paf"
+ECHO " Ficheiros copiados!"
+GOTO Exit
+
+:NotWindows
+ECHO Este ficheiro é para ser usado num SO Windows
 
 :Exit
+exit /b 0
